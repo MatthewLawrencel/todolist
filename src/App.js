496366e8e9221c315c58d3f350c6974afeebd005
MatthewLawrenceL <129269311+MatthewLawrencel@users.noreply.github.com>
2023-09-21 21:09:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import TodoApp from "./Components/Todo_App";
+
+import TodoApp from "./components/TodoApps";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
